26c8f9ac2d37524f4ad161649eb7740fbdec40bd
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-09-13 17:40:48 +0300

    feat(model): add ProductResponse class and implement mapping from Product

--- Respones/ProductResponse.cs
+++ Respones/ProductResponse.cs
@@ -1,27 +1,44 @@
 using System;
 public class ProductResponse
 {
-
-    public Guid Id { get; set; }
+    public Guid ProductId { get; set; }
     public string? Name { get; set; }
 
     public decimal Price { get; set; }
 
-    public List<ProductReviewResponse>? Reviews { get; set; }
+    public List<ProductReviewResponse>? Reviews { get; set; } = default;
+
 
     private ProductResponse() { }
 
-    public static ProductResponse FromModel(Product? product)
+    //map one product 
+
+    public static ProductResponse FromModel(Product product, IEnumerable<ProductReview>? reviews = null)
     {
         if (product is null)
-            throw new ArgumentNullException(nameof(product), "can not create a product for  null");
-        
-        return new ProductResponse{
-            Id = product.Id,
-            Name = product.Name, 
-            Price  = product.Price
+            throw new ArgumentNullException(nameof(product), "Can Not Create A Product Response From A Null Product");
+
+        var response = new ProductResponse
+        {
+            ProductId = product.Id,
+            Name = product.Name,
+            Price = product.Price
         };
 
+        if (reviews is not null)
+            response.Reviews = ProductReviewResponse.FromModel(reviews).ToList();
+
+        return response;
     }
 
+    //map list of  product 
+
+    public static IEnumerable<ProductResponse> FromModel(IEnumerable<Product> products)
+    {
+        if (products is null)
+            throw new ArgumentNullException(nameof(products), "Can Not Create A Product Response From A Null products");
+
+        return products.Select(p => FromModel(p));
     }
+
+}
