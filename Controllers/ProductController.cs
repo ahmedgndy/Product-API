887af140e1b009db7ec5f010ac4dccc4ca8222a0
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-09-13 22:19:47 +0300

    feat(route): add HEAD rout for product by id

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -4,12 +4,18 @@ using Microsoft.AspNetCore.Mvc;
 [ApiController]
 [Route("api/products")]
 
-public class ProductController : ControllerBase
+public class ProductController(ProductRepository repository) : ControllerBase
 {
     [HttpOptions]
     public IActionResult GetOptions()
     {
         Response.Headers.Append("Allow", "GET ,HEAD ,POST, DELETE ,PATCH ,OPTIONS");
         return NoContent();
+    }
+
+    [HttpHead("{productId:Guid}")]
+    public IActionResult GetOptions(Guid productId)
+    {
+        return repository.ExistsById(productId) ? Ok() : NotFound();
     }
 }
